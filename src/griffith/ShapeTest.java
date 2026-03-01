e4f5f5b402840640ad7ba7ec9d8c9ca66285c2cb
Mariia Kolo <marikolo57@gmail.com>
2026-03-01 13:31:31 +0000

    Wrote unit test testCircle, test failed

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -4,7 +4,30 @@
 package griffith;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ShapeTest {
+
+    private static final double EPSILON = 1.0;
+
+    @Test
+    void testCircle(){
+
+        Circle c = new Circle("Circle", 3);
+
+        assertEquals(28.27, c.area(), EPSILON);
+        assertEquals(18.85,  c.perimeter(), EPSILON);
+        assertTrue(c.toString().contains("Radius: "));
+    }
+
+    @Test
+    void testRhombus(){
+
+    }
+
+    @Test
+    void testTriangle(){
+
+    }
 
 }
