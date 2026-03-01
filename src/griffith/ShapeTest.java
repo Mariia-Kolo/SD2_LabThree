816741c0824b5d380e1cee60d0a80eaa3358b72e
Mariia Kolo <marikolo57@gmail.com>
2026-03-01 14:10:09 +0000

    wrote integration test, passed test

--- src/griffith/ShapeTest.java
+++ src/griffith/ShapeTest.java
@@ -5,6 +5,7 @@ package griffith;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 class ShapeTest {
 
@@ -42,4 +43,39 @@ class ShapeTest {
 
     }
 
+    @Test
+    void integrationTestShapes() {
+
+        ArrayList<Shape> shapes = new ArrayList<>();
+
+        //two of each shapes
+        shapes.add(new Circle("Circle", 3));
+        shapes.add(new Circle("Circle", 4));
+
+        shapes.add(new Rhombus("Rhombus", 6, 8, 5));
+        shapes.add(new Rhombus("Rhombus", 10, 6, 4));
+
+
+        shapes.add(new RightAngledTriangle("Triangle", 6, 8));
+        shapes.add(new RightAngledTriangle("Triangle", 12, 5));
+
+
+        double[] expectedAreas = {
+                28.27, 50.27,
+                24, 30,
+                24, 30
+        };
+
+        double[] expectedPerimeter = {
+                18.85, 25.13,
+                20, 16,
+                24, 30
+        };
+
+        for (int i = 0; i < shapes.size(); i++) {
+            assertEquals(expectedAreas[i], shapes.get(i).area(), EPSILON);
+            assertEquals(expectedPerimeter[i], shapes.get(i).perimeter(), EPSILON);
+        }
+
+    }
 }
